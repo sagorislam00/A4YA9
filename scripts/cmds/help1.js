330fcea19d02438a08e9515e323c985842f0e3a2
SAGOR_404 <babygithub@gmail.com>
2024-10-03 00:09:32 +0600

    Update help1.js

--- scripts/cmds/help1.js
+++ scripts/cmds/help1.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "[ 𝗔 𝗥 𝗬 𝗔 𝗡 ]"; // changing this wont change the goatbot V2 of list cmd it is just a decoyy
+const doNotDelete = "[ 𝗦 𝗔 𝗚 𝗢 𝗥 ]"; // changing this wont change the goatbot V2 of list cmd it is just a decoyy
 
 module.exports = {
   config: {
